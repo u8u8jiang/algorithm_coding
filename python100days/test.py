f3cde66bfcd11dd5f35d972c181a95325d445bfc
jill_jiang <jill_jiang@wistron.com>
2022-11-19 11:35:48 +0800

    ch3 mnist

--- python100days/test.py
+++ python100days/test.py
@@ -1,38 +1,14 @@
 
-import numpy as np
-
-def init_network():
-    network = {}
-    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
-    network['b1'] = np.array([0.1, 0.2, 0.3])
-    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
-    network['b2'] = np.array([0.1, 0.2])
-    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
-    network['b3'] = np.array([0.1, 0.2])
-    return network
-
-def sigmoid(x):
-    return 1/(1 + np.exp(-x) )
-def identity_fu(x):
-    return x
 
 
-def forward(netwark, x):
-    W1, W2, W3 = network['W1'], network['W2'], network['W3'] 
-    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+# MNIST dataset
+import sys, os
+sys.path.append(os.pardir)   #載入父目錄檔案設定
+from dataset.mnist import load_mnist
 
-    a1 = np.dot(x, W1) + b1
-    z1 = sigmoid(a1)
-    a2 = np.dot(z1, W2) + b2
-    z2 = sigmoid(a2)
-    a3 = np.dot(z2, W3) + b3
-    y = identity_fu(a3)
+(x_train, y_train), (x_test, y_test) = load_mnist(flatten=True, normalize=False)
 
-    return y
-
-
-network = init_network()
-x = np.array([1.0, 0.5])
-y = forward(network, x)
-print(y)
-    
+print(x_train.shape)
+print(y_train.shape)
+print(x_test.shape)
+print(y_test.shape)
